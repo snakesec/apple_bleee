79a7f10f24af09a66c42c55ddcd16d6ad21edbb6
Weidsom Nascimento <source@snakesecurity.org>
2025-05-22 16:11:30 -0700

    Update apple-blee source code to HARDBASED 5.0

---
+++ AFOSBUILD.sh
@@ -0,0 +1,52 @@
+rm -rf /opt/ANDRAX/apple_bleee
+
+python3 -m venv /opt/ANDRAX/apple_bleee
+
+source /opt/ANDRAX/apple_bleee/bin/activate
+
+/opt/ANDRAX/apple_bleee/bin/pip3 install wheel
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Pip install wheel... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+/opt/ANDRAX/apple_bleee/bin/pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Pip install PyBLUEZ... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+/opt/ANDRAX/apple_bleee/bin/pip3 install -r requirements.txt
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Install requirements... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf $(pwd) /opt/ANDRAX/apple_bleee/package
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf andraxbin/* /opt/ANDRAX/bin
+rm -rf andraxbin
